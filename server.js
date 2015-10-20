46e61de3f57883f7f0239937aa253087853bc6f2
nathaliesicard <nathalie.sicard@gmail.com>
2015-10-19 21:57:12 -0500

    Contact form update

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var Router = require('koa-router');
 var views = require('koa-views');
 
 var nunjucks = require('nunjucks');
-
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
 
 var app = koa();
 
@@ -54,4 +55,44 @@ var port = process.env.PORT || 2500;
 
 app.listen(port, function() {
     console.log('Great success! Listening on port ', port);
+});
+
+
+var transporter = nodemailer.createTransport(smtpTransport({
+    port: 587,
+    host: 'smtp.mandrillapp.com',
+    auth: {
+        user: 'contacto@maderaenmuebles.com',
+        pass: process.env.NODEMAILER_PASSWORD
+    }
+}));
+
+
+
+function sendEmail(text) {
+    return new Promise(function(resolve, reject) {
+        transporter.sendMail({
+            from: 'contacto@maderaenmuebles.com', // sender address
+            to: 'contacto@maderaenmuebles.com', // list of receivers
+            subject: 'Contacto Madera en Muebles SG',
+            text: text
+        }, function (error, info) {
+            if (error) {
+                console.error('Got send mail error: ', error);
+                reject(error);
+            } else {
+                console.log('Message sent: ', info.response);
+                resolve();
+            }
+        });
+    });
+}
+
+router.post('/contact', function*() {
+    console.log('Contact us form was run!', this.request.body);
+
+    yield sendEmail('A contact us was submitted with: ' + JSON.stringify(this.request.body));
+
+
+    this.body = 'El mensaje ha sido enviado. Gracias!';
 });
